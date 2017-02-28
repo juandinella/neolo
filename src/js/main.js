6d7d8a414b7910d277171b07f26283c395efbf70
Juan Di Nella <juan.dinella@gmail.com>
2017-02-28 19:29:31 -0300

    more pages

--- src/js/main.js
+++ src/js/main.js
@@ -160,3 +160,22 @@
     $(this).css('background', 'url(' + background + ') no-repeat center / cover')
   })
 }(jQuery))
+
+
+/**
+* Smooth Scroll
+*/
+$(function() {
+  $('a[href*="#"]:not([href="#"])').click(function() {
+    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+      if (target.length) {
+        $('html, body').animate({
+          scrollTop: target.offset().top - $('.mainHeader').outerHeight()
+        }, 1000);
+        return false;
+      }
+    }
+  });
+});
