a6595e7d89d709c1b049fd8dfc225dda8ffb6db1
Juan Di Nella <juan.dinella@gmail.com>
2017-06-06 20:30:13 -0300

    added analytics, fb pixel, full story and elev.io

---
+++ src/js/elev.js
@@ -0,0 +1,3 @@
+var _elev = window._elev || {};
+_elev.account_id = '587903536964f';
+(function () { var i,e;i=document.createElement('script'),i.type='text/javascript';i.async=1,i.src='https://cdn.elev.io/sdk/bootloader/v4/elevio-bootloader.js?cid=587903536964f',e=document.getElementsByTagName('script')[0],e.parentNode.insertBefore(i,e);})();
