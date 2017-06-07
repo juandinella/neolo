1fb4914aaaf9b45c77a9c2fc8d5a213d596f406d
Juan Di Nella <juan.dinella@gmail.com>
2017-06-06 22:19:35 -0300

    Update 2017-06-07T01:19:29.280Z

--- js/app.js
+++ js/app.js
@@ -1,3 +1,10 @@
+(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+})(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+ga('create', 'UA-47581672-1', 'auto');
+ga('send', 'pageview');
+
 /* global jQuery */
 
 ;(function ($) {
@@ -68,6 +75,10 @@
   })
 })(jQuery)
 
+var _elev = window._elev || {};
+_elev.account_id = '587903536964f';
+(function () { var i,e;i=document.createElement('script'),i.type='text/javascript';i.async=1,i.src='https://cdn.elev.io/sdk/bootloader/v4/elevio-bootloader.js?cid=587903536964f',e=document.getElementsByTagName('script')[0],e.parentNode.insertBefore(i,e);})();
+
 /* global jQuery */
 
 ;(function ($) {
@@ -114,6 +125,40 @@
     }
   })
 })(jQuery)
+
+script.
+  (function() {
+  var _fbq = window._fbq || (window._fbq = []);
+  if (!_fbq.loaded) {
+  var fbds = document.createElement('script');
+  fbds.async = true;
+  fbds.src = '//connect.facebook.net/en_US/fbds.js';
+  var s = document.getElementsByTagName('script')[0];
+  s.parentNode.insertBefore(fbds, s);
+  _fbq.loaded = true;
+  }
+  _fbq.push(['addPixelId', '732285840138667']);
+  })();
+  window._fbq = window._fbq || [];
+  window._fbq.push(['track', 'PixelInitialized', {}]);
+noscript
+  img(height='1', width='1', alt='', style='display:none', src='https://www.facebook.com/tr?id=732285840138667&ev=PixelInitialized')
+
+window['_fs_debug'] = false;
+window['_fs_host'] = 'www.fullstory.com';
+window['_fs_org'] = '2C83M';
+window['_fs_namespace'] = 'FS';
+(function(m,n,e,t,l,o,g,y){
+if (e in m && m.console && m.console.log) { m.console.log('FullStory namespace conflict. Please set window["_fs_namespace"].'); return;}
+g=m[e]=function(a,b){g.q?g.q.push([a,b]):g._api(a,b);};g.q=[];
+o=n.createElement(t);o.async=1;o.src='https://'+_fs_host+'/s/fs.js';
+y=n.getElementsByTagName(t)[0];y.parentNode.insertBefore(o,y);
+g.identify=function(i,v){g(l,{uid:i});if(v)g(l,v)};g.setUserVars=function(v){g(l,v)};
+g.identifyAccount=function(i,v){o='account';v=v||{};v.acctId=i;g(o,v)};
+g.clearUserCookie=function(c,d,i){if(!c || document.cookie.match('fs_uid=[`;`]`[`;`]`[`;`]*`')){
+d=n.domain;while(1){n.cookie='fs_uid=;domain='+d+
+';path=/;expires='+new Date(0).toUTCString();i=d.indexOf('.');if(i<0)break;d=d.slice(i+1)}}};
+})(window,document,window['_fs_namespace'],'script','user');
 
 /* global jQuery */
 
